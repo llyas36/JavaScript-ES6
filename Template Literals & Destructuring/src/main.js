ae8b36ab096d7160243ffd5b70c698b6d2720537
llyas36 <eliyasinbox1@gmail.com>
2025-06-20 21:38:17 +0300

    first version

---
+++ Template Literals & Destructuring/src/main.js
@@ -0,0 +1,10 @@
+// template litral
+// const name = "boo";
+// console.log(`Hi, This is ${name}`);
+
+// Destructuing...
+// array destructuing
+
+const myarray = [1, 2, 3, 4];
+const [, , , a, b] = myarray;
+console.log(a, b);

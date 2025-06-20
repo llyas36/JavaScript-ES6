fa5e43640468cde1cac7ae41db12f85454bb39e7
llyas36 <eliyasinbox1@gmail.com>
2025-06-20 22:38:22 +0300

    main.js modified

--- Template Literals & Destructuring/src/main.js
+++ Template Literals & Destructuring/src/main.js
@@ -1,10 +1,44 @@
 // template litral
-// const name = "boo";
-// console.log(`Hi, This is ${name}`);
+const name = "Lily";
+console.log(`Hello im ${name}`);
 
-// Destructuing...
-// array destructuing
+// Exptression Insertion
+const a = 5;
+const b = 15;
+console.log(`The sum:  ${a + b}`);
 
-const myarray = [1, 2, 3, 4];
-const [, , , a, b] = myarray;
-console.log(a, b);
+// Multi-line String
+
+const message = `
+  Hello
+  This is me
+  Writing in multi-line string`;
+console.log(message);
+
+//Destructuring...
+// Object Destructuring
+// Array Destructuring
+
+const person = {
+  fullName: "John Doe",
+  age: 36,
+  country: "United Kingdom",
+};
+
+// we can simply extract values using object destructuring
+
+const { fullName, country } = person;
+console.log(`Name: ${name}`);
+console.log(`Country: ${country}`);
+
+// renaming
+const { fullName: personName, country: personCountry } = person;
+console.log(`Full Name: ${personName}`);
+console.log(`Person Country: ${personCountry}`);
+
+//Array Destructuring...
+const colors = ["red", "black", "aqua", "blue"];
+const [first, , third] = colors;
+// we can skip elements in array
+console.log(first);
+console.log(third);
